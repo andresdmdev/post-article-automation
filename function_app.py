8db1124c422e094ff2831cdfa721367f2b5355a0
Andres Marquez <andresdmf55@gmail.com>
2025-02-12 21:18:49 -0500

    Modify function configuration to improve deployment with Github CLI

--- function_app.py
+++ function_app.py
@@ -1,4 +1,4 @@
-import logging,datetime,sys, os
+import logging, datetime, sys, os
 from azure.functions import FunctionApp, TimerRequest
 from utils.utils import save_input_as_md
 from flows.flow_generation_content.main import execute_content_generation_flow
@@ -9,13 +9,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 app = FunctionApp()
 
 @app.function_name(name="post_automation_trigger")
-@app.schedule(schedule="0 0 1 * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
-def post_automation_trigger(myTimer: TimerRequest) -> None:
-
+@app.timer_trigger(
+    schedule="0 0 0 1 * *",  # Formato cron de 6 campos (segundo incluido)
+    arg_name="myTimer",
+    run_on_startup=True
+)
+def main(myTimer: TimerRequest) -> None:
     result = execute_content_generation_flow()
-
     execute_social_network_content_flow()
-
     save_input_as_md(result, f"content_{datetime.datetime.now().year}_{datetime.datetime.now().month}.md")
-
-    logging.info('Python timer trigger function executed.')
+    logging.info('Trigger ejecutado correctamente a las %s', datetime.datetime.now().isoformat())
